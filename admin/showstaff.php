359702b15569db95edb8544201f50c03e15a47fa
alaaelgndy <alaaelgndy21@yahoo.com>
2015-04-23 16:21:03 +0200

    add delete of staff code

--- admin/showstaff.php
+++ admin/showstaff.php
@@ -64,37 +64,37 @@ require 'sidebar.php';
 
         ?>
 
-     
+        
 
-                <div class="row">
-                  <div class="col-xs-12">
-                    <div class="box">
-                      <div class="box-header">
-                       
-                        <div class="box-tools">
-                          <div class="input-group">
-                            <input type="text" name="table_search" class="form-control input-sm pull-right" style="width: 150px;" placeholder="Search"/>
-                            <div class="input-group-btn">
-                              <button class="btn btn-sm btn-default"><i class="fa fa-search"></i></button>
-                            </div>
-                          </div>
-                        </div>
-                      </div><!-- /.box-header -->
-                      <div class="box-body table-responsive no-padding">
-                        <table class="table table-hover">
-                          <tr>
-                            <th>ID</th>
-                            <th>NAME</th>
-                            <th>IMAGE</th>
-                            <th>TITLE</th>
-                            <th>DESCRIPTION</th>
-                            <th>FACEBOOK</th>
-                            <th>EMAIL</th>
-                            <th>USERNAME</th>
-                            <th>PASSWORD</th>
-                            <th>option</th>
-                          </tr>
-                        <?php require 'connection.php';
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="box">
+              <div class="box-header">
+               
+                <div class="box-tools">
+                  <div class="input-group">
+                    <input type="text" name="table_search" class="form-control input-sm pull-right" style="width: 150px;" placeholder="Search"/>
+                    <div class="input-group-btn">
+                      <button class="btn btn-sm btn-default"><i class="fa fa-search"></i></button>
+                    </div>
+                  </div>
+                </div>
+              </div><!-- /.box-header -->
+              <div class="box-body table-responsive no-padding">
+                <table class="table table-hover">
+                  <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>IMAGE</th>
+                    <th>TITLE</th>
+                    <th>DESCRIPTION</th>
+                    <th>FACEBOOK</th>
+                    <th>EMAIL</th>
+                    <th>USERNAME</th>
+                    <th>PASSWORD</th>
+                    <th>option</th>
+                  </tr>
+                  <?php require 'connection.php';
                         /*
                         show the data in staff table (about doctors and domestrator)
 
@@ -104,37 +104,37 @@ require 'sidebar.php';
                         while ($result=$query->fetch(PDO::FETCH_ASSOC)) {
                           extract($result);
                           echo "  <tr>
-                            <th>$i</th>
-                            <th>$name</th>
-                            <th><img src='image/".$image."' width='45' height='60'></th>
-                            <th>$title</th>
-                            <th>$description</th>
-                            <th>$facebook</th>
-                            <th>$email</th>
-                            <th>$username</th>
-                            <th>*********</th>
-                            <th><a href='deletestaff.php?id=$id' class='btn btn-danger btn-sm'>DELETE</a></th>
-                            
+                          <th>$i</th>
+                          <th>$name</th>
+                          <th><img src='image/".$image."' width='45' height='60'></th>
+                          <th>$title</th>
+                          <th>$description</th>
+                          <th>$facebook</th>
+                          <th>$email</th>
+                          <th>$username</th>
+                          <th>*********</th>
+                          <th><a href='deletestaff.php?id=$id&img=$image' class='btn btn-danger btn-sm'>DELETE</a></th>
+                          
                           </tr>";
                           $i++;
                         }
 
-                         ?>
-                          
-                        </table>
-                      </div><!-- /.box-body -->
-                    </div><!-- /.box -->
-                  </div>
+                        ?>
+                        
+                      </table>
+                    </div><!-- /.box-body -->
+                  </div><!-- /.box -->
                 </div>
-              </div><!-- /.box-body -->
-            </div><!-- /.box -->
-            <a href="subject.php"> <button class="btn btn-success btn-lg">insert new data</button></a>
+              </div>
+            </div><!-- /.box-body -->
+          </div><!-- /.box -->
+          <a href="subject.php"> <button class="btn btn-success btn-lg">insert new data</button></a>
 
-          </div>
         </div>
-      </section><!-- /.content --> 
+      </div>
+    </section><!-- /.content --> 
 
-      <?php
-      require 'footer.php'; 
-      ?>
+    <?php
+    require 'footer.php'; 
+    ?>
     
