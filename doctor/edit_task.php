8e5f7ecb34cacc38b2ff32794ed92b61a923de29
alaaelgndy <alaaelgndy21@yahoo.com>
2015-04-28 22:48:36 +0200

    add code php to edit task

--- doctor/edit_task.php
+++ doctor/edit_task.php
@@ -25,15 +25,23 @@ require 'header.php';
                     <div class="box-header">
                         <h3 class="box-title">Add New Task</h3>
                     </div>
+                    <?php
+                     require '../admin/connection.php';
+                     $query3=$conn->query("SELECT * FROM tasks");
+                     $result3=$query3->fetch(PDO::FETCH_ASSOC);
+                     extract($result3);
+                      if (isset($_GET['task_id'])) {
+                        $id=$_GET['task_id'];
+                    } ?>
 
                     <!-- form start -->
-                    <form role="form">
+                    <form role="form" action="update_task<?php echo "?id=$id"; ?>" method="POST" enctype="multipart/form-data" >
                         <div class="box-body">
 
                             <div class="row">
                                 <div class="col-md-6"> <div class="form-group">
                                     <label for="exampleInputEmail1">Task Title</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Task Title">
+                                    <input type="text" name='title' value='<?php echo "$task_title"; ?>' class="form-control" id="exampleInputEmail1" placeholder="Enter Task Title">
                                 </div>
 
                                 <div class="form-group files">
@@ -49,67 +57,75 @@ require 'header.php';
                                         <div class="input-group-addon">
                                             <i class="fa fa-calendar"></i>
                                         </div>
-                                        <input type="text" class="form-control pull-right" id="reservation"/>
+                                        <input type="text" name='date' value='<?php echo "from: $created_at to: $ex_date"; ?>' class="form-control pull-right" id="reservation"/>
                                     </div><!-- /.input group -->
                                 </div><!-- /.form group -->
 
 
                             </div>
                             <div class="col-md-6">
-                               <div class="form-group">
+                             <div class="form-group">
                                 <label>Department</label>
-                                <select class="form-control">
-                                    <option>option 1</option>
-                                    <option>option 2</option>
-                                    <option>option 3</option>
-                                    <option>option 4</option>
-                                    <option>option 5</option>
-                                </select>
-                            </div>
+                                <select class='form-control' name='department'>
+                                    <?php 
+                                    $sql="SELECT * FROM departments";
+                                    $query=$conn->query($sql);
+                                    while ($result=$query->fetch(PDO::FETCH_ASSOC)) {
+                                     extract($result);
+                                     echo "<option value='$id'>$name</option>";
 
-                            <div class="form-group">
-                                <label>Year </label>
-                                <select class="form-control">
-                                    <option>option 1</option>
-                                    <option>option 2</option>
-                                    <option>option 3</option>
-                                    <option>option 4</option>
-                                    <option>option 5</option>
-                                </select>
-                            </div>
+                                 }
 
-                            <div class="form-group">
-                                <label>Subject</label>
-                                <select class="form-control">
-                                    <option>option 1</option>
-                                    <option>option 2</option>
-                                    <option>option 3</option>
-                                    <option>option 4</option>
-                                    <option>option 5</option>
-                                </select>
-                            </div>
+                                 ?>
+                             </select>
+
+                         </div>
+
+                         <div class="form-group">
+                            <label>Year </label>
+                            <select class="form-control" name='year'>
+                                <option value='1'>year1</option>
+                                <option value='2'>year2</option>
+                                <option value='3'>year3</option>
+                                <option value='4'>year4</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Subject</label>
+                            <select class="form-control" name='subject'>
+                                <?php 
+                                $query2=$conn->query("SELECT * FROM subjects");
+                                while ($result2=$query2->fetch(PDO::FETCH_ASSOC)) {
+                                    extract($result2); 
+                                    echo "<option value='$id'>$name</option>";
+                                }
+
+                                ?>
+                            </select>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                           <label>Task Content</label>
-                           <textarea class="textarea"  placeholder="Place some text here" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-                       </div>
-                   </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                     <label>Task Content</label>
+                     <textarea class="textarea"  name='content' placeholder="Place some text here" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"><?php echo "$task_content"; ?></textarea>
+                 </div>
+             </div>
 
 
 
-               </div><!-- /.box-body -->
+         </div><!-- /.box-body -->
 
-               <div class="box-footer">
-                <input type="submit" class="btn btn-success" value="Edit Task">
-            </div>
-        </form>
-
-
+         <div class="box-footer">
+            <input type="submit" class="btn btn-success" value="Edit Task">
+        </div>
+    </form>
 
 
-    </div>
+
+
+</div>
 </div>
 </div>
 </section><!-- /.content -->
@@ -120,7 +136,7 @@ require 'header.php';
 <script src="../admin/js/bootstrap.min.js" type="text/javascript"></script>
 <!-- AdminLTE App -->
 <script src="../admin/js/AdminLTE/app.js" type="text/javascript"></script>
- <script src="../admin/js/AdminLTE/dashboard.js" type="text/javascript"></script>
+<script src="../admin/js/AdminLTE/dashboard.js" type="text/javascript"></script>
 <!-- Bootstrap WYSIHTML5 -->
 
 <!-- date-range-picker -->
